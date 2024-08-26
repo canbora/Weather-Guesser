67c73ca743e1c9f69280f012668c4d033fe708d7
canbora <canboraugur@gmail.com>
2024-08-26 15:43:53 +0300

    add script to resize all images to common value and save as numpy array

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This project will consist of a program that, given a photograph, guesses what th
 I left the dataset out of the repository because it was too large. You can download it from [Kaggle](https://www.kaggle.com/datasets/jehanbhathena/weather-dataset/data). The weather folders should then be put in the dataset folder.
 
 I am currently preparing this project as part of my internship at Turkcell Communication Services, where I was tasked to implement a program performing image classification.
+
+## Running
+
+- To regenerate images.npz, which holds normalized image data, run `imageresizer.py`. The dataset needs to be installed to run this program.
---
+++ imageresizer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from PIL import Image
+import os
+import numpy as np
+import random
+
+weather_types = ["dew", "fogsmog", "frost", "glaze", "hail", "lightning", "rain", "rainbow", "rime", "sandstorm", "snow"]
+
+"""
+Resizes all images in the database to a side x side square by cropping the ends of the longer side and scaling.
+Formats the resulting images, and the corresponding weathers, as two numpy arrays.
+Saves them in images.npz.
+"""
+def resize_images(side):
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    # Each element is of the form (image, weather_index)
+    images = []
+    for weather_index, weather_name in enumerate(weather_types):
+        for file in os.scandir(f"{file_path}/dataset/{weather_name}"):
+            im = Image.open(file.path)
+
+            # Crop the longer side
+            width, height = im.size
+            if width <= height:
+                im = im.crop((0, (height - width)//2, width, (height + width)//2))
+            else:
+                im = im.crop(((width - height)//2, 0, (width + height)//2, height))
+
+            im = im.resize((side, side))
+            im = im.convert("RGB")
+            imdata = np.asarray(im)
+            images.append((imdata, weather_index))
+    random.shuffle(images)
+    np.savez("images.npz", images=np.array([item[0] for item in images]), weather=np.array([item[1] for item in images]))
+
+
+if __name__ == "__main__":
+    side = "None"
+    while not side.isdigit():
+        side = input("Resim kenarı kaç pixel olsun? : ")
+    resize_images(int(side))
