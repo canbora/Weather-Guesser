edd662a4aa83a4c6e1e791cfaed5e4b9596d2666
canbora <canboraugur@gmail.com>
2024-08-29 15:15:11 +0300

    add function to prepare data for single image

--- imageresizer.py
+++ imageresizer.py
@@ -6,6 +6,22 @@ import random
 
 weather_types = ["dew", "fogsmog", "frost", "glaze", "hail", "lightning", "rain", "rainbow", "rime", "sandstorm", "snow"]
 
+
+def get_single_image_data(path, side):
+    im = Image.open(path)
+
+    # Crop the longer side
+    width, height = im.size
+    if width <= height:
+        im = im.crop((0, (height - width)//2, width, (height + width)//2))
+    else:
+        im = im.crop(((width - height)//2, 0, (width + height)//2, height))
+
+    im = im.resize((side, side))
+    im = im.convert("RGB")
+    imdata = np.asarray(im)
+    return imdata
+
 """
 Resizes all images in the database to a side x side square by cropping the ends of the longer side and scaling.
 Formats the resulting images, and the corresponding weathers, as two numpy arrays.
@@ -17,18 +33,7 @@ def resize_images(side):
     images = []
     for weather_index, weather_name in enumerate(weather_types):
         for file in os.scandir(f"{file_path}/dataset/{weather_name}"):
-            im = Image.open(file.path)
-
-            # Crop the longer side
-            width, height = im.size
-            if width <= height:
-                im = im.crop((0, (height - width)//2, width, (height + width)//2))
-            else:
-                im = im.crop(((width - height)//2, 0, (width + height)//2, height))
-
-            im = im.resize((side, side))
-            im = im.convert("RGB")
-            imdata = np.asarray(im)
+            imdata = get_single_image_data(file.path, side)
             images.append((imdata, weather_index))
     random.shuffle(images)
     np.savez("images.npz", images=np.array([item[0] for item in images]), weather=np.array([item[1] for item in images]))
