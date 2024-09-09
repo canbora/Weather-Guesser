70ece5ddaa07ba2fcf980ef5fc18170e48ed1ee4
canbora <canboraugur@gmail.com>
2024-09-09 16:43:36 +0300

    better format results in GUI

--- gui.py
+++ gui.py
@@ -14,8 +14,12 @@ def load_image():
     image = Image.open(image_filename)
     image.thumbnail((250, 250))
     selected_image = ImageTk.PhotoImage(image)
+    table_results.config(show="headings")
+    table_results.delete(*table_results.get_children())
+    for (result, category) in results:
+        table_results.insert("", "end", values=(category, f"{result:.3f}"))
     formatted_results = "\n".join(f"    {category}: {result:.3f}" for result, category in results)
-    label_image.config(text=formatted_results, image=selected_image)
+    label_image.config(image=selected_image)
 
 
 # Window creation and geometry
@@ -24,7 +28,7 @@ window = tk.Tk()
 window.title("Weather Guesser")
 
 ## Center window
-width = 500
+width = 540
 height = 360
 offset_x = (window.winfo_screenwidth()-width)//2
 offset_y = (window.winfo_screenheight()-height)//2
@@ -35,7 +39,12 @@ window.geometry(f"{width}x{height}+{offset_x}+{offset_y}")
 button_load_image = ttk.Button(window, text="Test an Image", command=load_image, padding=10)
 button_load_image.pack(padx=10, pady=10)
 selected_image = None
-label_image = ttk.Label(window, text="", compound="left", font=("Cambria", 12), padding=10)
-label_image.pack(padx=10, pady=10)
+label_image = ttk.Label(window, font=("Cambria", 12), padding=10)
+label_image.pack(side=tk.LEFT, padx=10, pady=10)
+
+table_results = ttk.Treeview(window, show="", columns=("category", "probability"), height=11)
+table_results.heading("category", text="Weather")
+table_results.heading("probability", text="Probability")
+table_results.pack(side=tk.RIGHT, padx=20, pady=20)
 
 window.mainloop()
