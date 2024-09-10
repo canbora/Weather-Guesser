fb8ebf9cbe799b133f8bcb2eff78932a304182bd
canbora <canboraugur@gmail.com>
2024-09-10 15:58:01 +0300

    switch GUI code to object oriented style

--- gui.py
+++ gui.py
@@ -5,46 +5,56 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from predict import predict
 
-# Functions
+class ResultsFrame(ttk.Frame):
 
-def load_image():
-    global selected_image
-    image_filename = filedialog.askopenfilename(multiple=False)
-    results = predict(image_filename)
-    image = Image.open(image_filename)
-    image.thumbnail((250, 250))
-    selected_image = ImageTk.PhotoImage(image)
-    table_results.config(show="headings")
-    table_results.delete(*table_results.get_children())
-    for (result, category) in results:
-        table_results.insert("", "end", values=(category, f"{result:.3f}"))
-    formatted_results = "\n".join(f"    {category}: {result:.3f}" for result, category in results)
-    label_image.config(image=selected_image)
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.label_image = ttk.Label(self, font=("Cambria", 12))
+        self.label_image.pack(side=tk.LEFT, padx=20, pady=20, expand=True)
+
+        self.table_results = ttk.Treeview(self, show="", columns=("category", "probability"), height=11)
+        self.table_results.heading("category", text="Weather")
+        self.table_results.heading("probability", text="Probability")
+        self.table_results.pack(side=tk.LEFT, padx=20, pady=20, expand=True)
+
+    def update(self, image_filename, results):
+        image = Image.open(image_filename)
+        image.thumbnail((250, 250))
+        self.tk_image = ImageTk.PhotoImage(image)
+        self.table_results.config(show="headings")
+        self.table_results.delete(*self.table_results.get_children())
+        for (result, category) in results:
+            self.table_results.insert("", "end", values=(category, f"{result:.3f}"))
+        self.label_image.config(image=self.tk_image)
 
 
-# Window creation and geometry
+class Window(tk.Tk):
 
-window = tk.Tk()
-window.title("Weather Guesser")
+    def __init__(self):
 
-## Center window
-width = 540
-height = 360
-offset_x = (window.winfo_screenwidth()-width)//2
-offset_y = (window.winfo_screenheight()-height)//2
-window.geometry(f"{width}x{height}+{offset_x}+{offset_y}")
+        super().__init__()
+        self.title("Weather Guesser")
 
-# Widgets
+        ## Center window
+        width = 540
+        height = 360
+        offset_x = (self.winfo_screenwidth()-width)//2
+        offset_y = (self.winfo_screenheight()-height)//2
+        self.geometry(f"{width}x{height}+{offset_x}+{offset_y}")
 
-button_load_image = ttk.Button(window, text="Test an Image", command=load_image, padding=10)
-button_load_image.pack(padx=10, pady=10)
-selected_image = None
-label_image = ttk.Label(window, font=("Cambria", 12), padding=10)
-label_image.pack(side=tk.LEFT, padx=10, pady=10)
+        # Widgets
+        self.button_load_image = ttk.Button(self, text="Test an Image", command=self.load_image, padding=10)
+        self.button_load_image.pack(padx=10, pady=10)
 
-table_results = ttk.Treeview(window, show="", columns=("category", "probability"), height=11)
-table_results.heading("category", text="Weather")
-table_results.heading("probability", text="Probability")
-table_results.pack(side=tk.RIGHT, padx=20, pady=20)
+        self.results_frame = ResultsFrame(self)
+        self.results_frame.pack()
 
+    def load_image(self):
+        image_filename = filedialog.askopenfilename(multiple=False)
+        results = predict(image_filename)
+        self.results_frame.update(image_filename, results)
+
+
+window = Window()
 window.mainloop()
